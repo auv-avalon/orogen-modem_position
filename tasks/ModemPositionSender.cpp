b5af5f536c27e8703a1130f6a54c95156a3fa065
Niklas Pech <niklas.pech@dfki.de>
2014-09-16 11:05:00 +0200

    first untested version

--- tasks/ModemPositionSender.cpp
+++ tasks/ModemPositionSender.cpp
@@ -39,6 +39,26 @@ bool ModemPositionSender::startHook()
 void ModemPositionSender::updateHook()
 {
     ModemPositionSenderBase::updateHook();
+    base::samples::RigidBodyState rbs;
+    if (_position_samples.readNewest(rbs) == RTT::NewData){
+        //Message Type 0
+        if (rbs.position[0] < -16 || rbs.position[0] > 0 || rbs.position[1] < 0 || rbs.position[1] > 64){
+            return;
+            //Go in Error State
+        }
+        modemdriver::modem_message out_message;
+        std::vector<char> message(2);
+        uint8_t message_type = 1; 
+        message[0] = 0;
+        message[0] = message[0] || message_type<<6;
+        message[0] = message[0] || ((int) round(rbs.position[0]*2))<<1;
+        message[0] = message[0] || ((int) (rbs.position[1]*2)) >> 7;
+        message[1] = 0;
+        message[1] = message[1] || ((int) (rbs.position[1]*2)) & 0x7F;
+        out_message.time = base::Time::now();
+        out_message.message = std::string(message.begin(), message.end());
+        _modem_data.write(out_message);
+    }
 }
 void ModemPositionSender::errorHook()
 {
